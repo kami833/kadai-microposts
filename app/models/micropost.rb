3d1808bf05252a672d2cbfae2957356c1a3b4906
kame.mi <amenomori.minakanusi.kamim@gmail.com>
2020-06-15 12:22:00 +0000

    fixed dependent

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,8 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
   
-  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'user_id', dependent: :destroy
-  has_many :users, through: :reverses_of_favorite
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites
   
   validates :content, presence: true, length: { maximum: 255 }
 end
